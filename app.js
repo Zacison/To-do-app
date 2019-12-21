67250e3e2241453da3c99678e76f45e81ce82e05
Zach Dantzer <52724062+Zacison@users.noreply.github.com>
2019-12-20 23:22:00 -0600

    First commit
    I started this todo app by creating the base of the HTML and CSS. There is still a lot of CSS & JS to add to make it fully interactive, but to start with, I added in the MVC pattern in the JS file so I can continue to practice this design pattern.

---
+++ app.js
@@ -0,0 +1,31 @@
+var dataModule = (function() {
+
+
+})();
+
+
+var UIModule = (function(){
+
+
+
+})();
+
+
+var appControllerModule = (function(dataModule, UIModule){
+
+	document.querySelector('.input-button').addEventListener('click', function(){
+		//What needs to happen when the button is pressed?
+
+		//Read input from user interface & store as a var
+		
+		//create a data model, and add it to the data model
+
+		//add to user interface
+
+		console.log("hello");
+
+	})
+
+	
+
+})(dataModule, UIModule);
