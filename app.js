a1cb2700553603f4f67914fda17e9052170915ee
Zach Dantzer <52724062+Zacison@users.noreply.github.com>
2019-12-21 22:16:00 -0600

    3rd commit, added deletion functionality
    Added deletion functionality so that it works properly. Fixed a bug where the class that holds the list would still there, leading to a build up of empty white spaces. Also fixed a bug that deleted only the most recent item, no matter what item you pressed the delete button on. The core functionality is now done, and just the styling & the checkmark text styling is left to do.

--- app.js
+++ app.js
@@ -26,6 +26,27 @@ var dataModule = (function() {
 
 			return newItem;
 		},
+
+
+		deleteItemData: function(idNum) {
+
+			var idArray = data.allItems.map(function(currentValue){
+				return currentValue.id;
+			});
+
+			var index = idArray.indexOf(idNum);
+
+			if(index !== -1) {
+				data.allItems.splice(index, 1);
+			}
+			
+		},
+
+		allDataItems: function() {
+			return {
+				allItems: data.allItems,
+			}
+		}
 	}
 
 })();
@@ -44,22 +65,41 @@ var UIModule = (function(){
 			var html;
 			var newHtml;
 			var firstItemHtml;
+		
 
-			if (item.id === 0) {
-				
-				html = '<section class="to-do-list"><section class="list-items"><div class="item" id="item-0"><span class="checkmark-button"><input type="checkbox"></span><span class="item-description"><h2>%description%</h2></span><span class="delete-button"><input type="button" value= "delete" class="actual-delete-button"></span></div></section></section>';
-				newHtml = html.replace('%description%', item.description);
+				if (item.id === 0) {
+					
+					html = '<section class="to-do-list"><section class="list-items"><div class="item" id="item-0"><span class="checkmark-button"><input type="checkbox"></span><span class="item-description"><h2>%description%</h2></span><span class="delete-button"><input type="button" value= "delete" class="actual-delete-button"></span></div></section></section>';
+					newHtml = html.replace('%description%', item.description);
 
-				document.querySelector('.to-do-list-container').insertAdjacentHTML('beforeend', newHtml);
-			} 
-			else {
+					document.querySelector('.to-do-list-container').insertAdjacentHTML('beforeend', newHtml);
+				} 
+			
+				else {
 
-				html = '<div class="item" id="item-0"><span class="checkmark-button"><input type="checkbox"></span><span class="item-description"><h2>%description%</h2></span><span class="delete-button"><input type="button" value= "delete" class="actual-delete-button"></span></div>';
-				newHtml = html.replace('%description%', item.description);
+					html = '<div class="item" id="item-0"><span class="checkmark-button"><input type="checkbox"></span><span class="item-description"><h2>%description%</h2></span><span class="delete-button"><input type="button" value= "delete" class="actual-delete-button"></span></div>';
+					newHtml = html.replace('%description%', item.description);
+					newHtml = newHtml.replace('item-0', 'item-' + item.id);
 
-				document.querySelector('.list-items').insertAdjacentHTML('beforeend', newHtml);		
-			}
+					document.querySelector('.list-items').insertAdjacentHTML('afterbegin', newHtml);		
+				}
 		},
+
+		clearFields: function() {
+			document.querySelector('.input-text').value = "";
+		},
+
+
+		deleteToDoListItem: function(selectorID) {
+			var el = document.getElementById(selectorID);
+			el.parentNode.removeChild(el);
+		},
+
+		deleteLastToDoListItem: function(selectorID) {
+			var el = document.querySelector('.' + selectorID);
+			el.parentNode.removeChild(el);
+		},
+
 	}
 
 })();
@@ -81,17 +121,64 @@ var appControllerModule = (function(dataModule, UIModule){
 		//#3: add to user interface
 		UIModule.displayItem(newItem);
 
+		//#4: Clear the fields
+		UIModule.clearFields();
+	};
+
+
+
+	var deleteItem = function(event) {
+
+		//#1: Listen to the event firing to delete an element
+	
+		var itemID = event.target.parentNode.parentNode.id;
+		
+		var classID = event.target.className; //.actual delete button
+		
+		var toDoListClass = event.target.parentNode.parentNode.parentNode.parentNode.className; //.to-to-list 
+		
+		var howManyItems = dataModule.allDataItems().allItems.length - 1;
+		
+
+		var splitID = itemID.split('-');
+		var id = parseInt(splitID[1]);
+	
+		if(itemID && classID === "actual-delete-button") {
+
+				//#2: Delete item from data strucute
+				dataModule.deleteItemData(id);
+
+				//#3: Delete item from UI
+				UIModule.deleteToDoListItem(itemID);
+			
+		}
+		 if (howManyItems == 0) {
+			console.log("Baby yoda");
+
+			//#1: Delete last item from data structure
+			dataModule.deleteItemData(id);
+				
+
+			//#3: Delete the last item from UI
+			UIModule.deleteLastToDoListItem(toDoListClass);
+				
+		}
 	}
+
+
 
 	var setupEventListeners = function() {
 		document.querySelector('.input-button').addEventListener('click', function(){
 			addNewItem();		
 		});
+		
 		document.addEventListener('keypress', function(event){
 			if(event.keycode === 13 || event.which === 13) {
 				addNewItem();
 			}
 		});
+
+		document.querySelector('.to-do-list-container').addEventListener('click', deleteItem);
 	};
 
 	return {
